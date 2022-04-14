1b5e552165ac63585cc3ac5c26d0aeb27e5fbd25
Viktar Dabrou <VDobrov@alfabank.kz>
2022-04-14 18:06:52 +0300

    master/1.0 -initialize app

--- app/src/main/java/com/example/recipeapp/MainActivity.kt
+++ app/src/main/java/com/example/recipeapp/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.recipeapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            Column(
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Text(text = "What's up")
+                Spacer(modifier = Modifier.padding(top = 10.dp))
+                Button(onClick = { /*TODO*/ }) {
+                    Text(text = "Button Click")
+                }
+            }
+        }
     }
 }
